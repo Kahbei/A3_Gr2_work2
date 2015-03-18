a42228e968f060aaf5f9055d06b40c2656344e64
Gary MENGUS <mengus.gary@gmail.com>
2015-03-18 17:50:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,6 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-/*spl_autoload_register(function($name){
-    $file = $name.'.php';
-    if(is_file($file))
-        include $file;
-});*/
-
-//$slugify = new \Cocur\Slugify\Slugify();
-
-//echo $slugify->slugify('Mé àré azskj dz ak azd HJVJV');
-
 echo \OKLM\StringGenerator\PasswordGenerator::generate(12, \OKLM\StringGenerator\PasswordGenerator::PASSWORD_MEDIUM).'<br>';
 
 echo \OKLM\StringGenerator\LoremIpsum::generateMyLatin(10);
